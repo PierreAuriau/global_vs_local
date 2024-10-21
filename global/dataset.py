17b0732942df6bc4d8f47854d8d467b8dfc65ad2
PierreAuriau <p.auriau@hotmail.fr>
2024-10-21 14:51:09 +0200

    add clinical dataset

--- global/dataset.py
+++ global/dataset.py
@@ -3,9 +3,10 @@
 
 import os
 import logging
+import pickle
 import pandas as pd
 import numpy as np
-
+import bisect
 from typing import Callable, List, Type, Sequence, Dict, Union
 import torch
 from torch.utils.data.dataset import Dataset
@@ -19,7 +20,8 @@ class UKBDataset(Dataset):
 
     def __init__(self, split: str = 'train', 
                  label: str = None, 
-                 transforms: Callable[[np.ndarray], np.ndarray] = None, 
+                 transforms: Callable[[np.ndarray], np.ndarray] = None,
+                 target_mapping: dict = None, 
                  two_views: bool = False):
         """
         :param target: str or [str], either 'sex' or 'age'.
@@ -37,16 +39,21 @@ class UKBDataset(Dataset):
         self.scheme = self.load_scheme()
 
         # 2) Selects the data to load in memory according to selected scheme
-        mask = self._extract_mask(self.metadata, unique_keys=self._unique_keys, check_uniqueness=True)
+        assert (self.scheme[self._unique_keys] == self.metadata[self._unique_keys]).all(), \
+        f"Scheme and participant dataframes do not have same order"
+        mask = (self.scheme["set"] == self.split)
         self.metadata = self.metadata[mask]
 
         # 3) Get the labels to predict
         if label is not None:
             self.label = label
-            assert self.label in self.metadata.colums(), \
+            assert self.label in self.metadata.columns, \
                 f"Inconsistent files: missing {self.label} in participants DataFrame"
-            self.target = self.metadata[self.label].values.astype(np.float32)
+            self.target = self.metadata[self.label]
+            if target_mapping is not None:
+                self.target = self.target.replace(target_mapping)
             assert self.target.isna().sum().sum() == 0, f"Missing values in {self.label} column"
+            self.target = self.target.values.astype(np.float32)
         else:
             self.target = None
 
@@ -64,24 +71,10 @@ class UKBDataset(Dataset):
                 "session": int,
                 "acq": int,
                 "run": int}
-
-    def _extract_mask(self, df: pd.DataFrame, unique_keys: Sequence[str], check_uniqueness: bool = True):
-        """
-        :param df: pandas DataFrame
-        :param unique_keys: list of str
-        :param check_uniqueness: if True, check the unique_keys identified uniquely an image in the dataset
-        :return: a binary mask indicating, for each row, if the participant belongs to the current scheme or not.
-        """
-        _source_keys = df[unique_keys].apply(lambda row: '_'.join(row.values.astype(str)), axis=1)
-        if check_uniqueness:
-            assert len(set(_source_keys)) == len(_source_keys), f"Multiple identique identifiers found"
-        _target_keys = self.scheme[unique_keys].apply(lambda row: '_'.join(row.values.astype(str)), axis=1)
-        mask = _source_keys.isin(_target_keys).values.astype(bool)
-        return mask
     
     def load_scheme(self):
         scheme_df = pd.read_csv(os.path.join(config.path2schemes, self._train_val_scheme), dtype=self._id_types)
-        return scheme_df.loc[scheme_df["set"] == self.split, self._unique_keys]
+        return scheme_df[self._unique_keys]
     
     def __getitem__(self, idx: int):
         sample = dict()
@@ -105,8 +98,136 @@ class UKBDataset(Dataset):
 
     def __str__(self):
         return f"UKBDataset({self.split} set)"
+
+
+class ClinicalDataset(Dataset):
+
+    def __init__(self, dataset: str, split: str = 'train', 
+                 label: str = "diagnosis", 
+                 transforms: Callable[[np.ndarray], np.ndarray] = None,
+                 target_mapping: dict = None):
+        """
+        :param dataset: str, either 'asd', 'bd' or 'scz'
+        :param label: str, either 'diagnosis', 'sex' or 'age'.
+        :param split: str, either 'train', 'validation'
+        :param transforms: Callable, data transformations
+        :param target_mapping: dict, mapping between labels and int to predict
+        """
+        # 0) set attributes
+        self.dataset = dataset
+        self.split = split
+        self.transforms = transforms
+
+        # 1) Loads globally all the data
+        self.metadata = pd.concat([pd.read_csv(os.path.join(config.path2clinicaldata, f"{s}_t1mri_skeleton_participants.csv")) 
+                                   for s in self._studies],
+                       ignore_index=True, sort=False)
+        self.scheme = self.load_scheme()
+        self.images = [np.load(os.path.join(config.path2clinicaldata, f"{s}_t1mri_skeleton_data32.npy"), mmap_mode='r')
+                       for s in self._studies]
+        
+        # 2) Selects the data to load in memory according to selected scheme
+        mask = self._extract_mask(self.metadata, unique_keys=self._unique_keys, check_uniqueness=True)
+        self.metadata = self.metadata[mask]
+
+        # 3) Get the labels to predict
+        if label is not None:
+            self.label = label
+            assert self.label in self.metadata.columns, \
+                f"Inconsistent files: missing {self.label} in participants DataFrame"
+            self.target = self.metadata[self.label]
+            if target_mapping is not None:
+                self.target = self.target.replace(target_mapping)
+            assert self.target.isna().sum().sum() == 0, f"Missing values in {self.label} column"
+            self.target = self.target.values.astype(np.float32)
+        else:
+            self.target = None
+
+        # Attributes for mapping idx
+        self._mask_indices = np.arange(len(mask))[mask]
+        self._cumulative_sizes = np.cumsum([len(arr) for arr in self.images])
+
+    @property
+    def _studies(self) -> List:
+        if self.dataset == "asd":
+            return ["abide1", "abide2"]
+        elif self.dataset == "bd":
+            return ["biobd", "bsnip1", "cnp", "candi"]
+        elif self.dataset == "scz":
+            return ["schizconnect-vip-prague", "bsnip1", "cnp", "candi"]
+
+    @property
+    def _train_val_scheme(self) -> str:
+        if self.dataset == "asd":
+            return "train_val_test_test-intra_asd_stratified.pkl"
+        elif self.dataset == "bd":
+            return "train_val_test_test-intra_bd_stratified.pkl"
+        elif self.dataset == "scz":
+            return "train_val_test_test-intra_scz_stratified.pkl"
+
+    @property
+    def _unique_keys(self) -> List[str]:
+        if self.dataset == "asd":
+            return ["participant_id", "session", "run", "study"]
+        elif self.dataset in ["bd", "scz"]:
+            return ["participant_id", "session", "study"]
+    
+    @property
+    def _id_types(self):
+        return {"participant_id": str,
+                "session": int,
+                "acq": int,
+                "run": int}
+
+    def _extract_mask(self, df: pd.DataFrame, unique_keys: Sequence[str], check_uniqueness: bool = True):
+        """
+        :param df: pandas DataFrame
+        :param unique_keys: list of str
+        :param check_uniqueness: if True, check the unique_keys identified uniquely an image in the dataset
+        :return: a binary mask indicating, for each row, if the participant belongs to the current scheme or not.
+        """
+        _source_keys = df[unique_keys].apply(lambda row: '_'.join(row.values.astype(str)), axis=1)
+        if check_uniqueness:
+            assert len(set(_source_keys)) == len(_source_keys), f"Multiple identique identifiers found"
+        _target_keys = self.scheme[unique_keys].apply(lambda row: '_'.join(row.values.astype(str)), axis=1)
+        mask = _source_keys.isin(_target_keys).values.astype(bool)
+        return mask
+    
+    def load_scheme(self):
+        with open(os.path.join(config.path2schemes, self._train_val_scheme), "rb") as f:
+            scheme = pickle.load(f)
+        return scheme[self.split]
+    
+    def _mapping_idx(self, idx: int):
+        """
+        :param idx: int ranging from 0 to len(dataset)-1
+        :return: integer that corresponds to the original image index to load
+        """
+        idx = self._mask_indices[idx]
+        dataset_idx = bisect.bisect_right(self._cumulative_sizes, idx)
+        sample_idx = idx - self._cumulative_sizes[dataset_idx - 1] if dataset_idx > 0 else idx
+        return (dataset_idx, sample_idx)
+    
+    def __getitem__(self, idx: int):
+        sample = dict()
+        if self.target is not None:
+            sample[self.label] = self.target[idx]
+        (dataset_idx, sample_idx) = self._mapping_idx(idx)
+        arr = self.images[dataset_idx][sample_idx]
+        if self.transforms is not None:
+            sample["input"] = self.transforms(arr)
+        else:
+            sample["input"] = arr
+        return sample
+    
+    def __len__(self):
+        return len(self.metadata)
+
+    def __str__(self):
+        return f" {self.dataset.upper()}Dataset({self.split} set)"
     
 if __name__ == "__main__":
+    
     train_dataset = UKBDataset(split="train")
     val_dataset = UKBDataset(split="validation")
     assert (len(train_dataset) + len(val_dataset) == 21045), "Wrong number of subjects"
@@ -115,3 +236,36 @@ if __name__ == "__main__":
     assert np.all(item["input"].shape == (128, 160, 128)), "Wrong image shape"
     assert set(np.unique(item["input"])).issubset({0, 1}), "Wrong values in skeleton"
     assert item["input"].dtype == np.float32, "Wrong data type"
+    
+    for split, n in zip(("train", "validation", "test_intra", "test"),
+                        (1299, 163, 161, 116)):
+        asddataset = ClinicalDataset(dataset="asd", split=split,
+                                    target_mapping={"asd": 1, "control": 0})
+        assert len(asddataset) == n, "Wrong number of subjects"
+        item = asddataset[54]
+        assert np.all(set(asddataset.target).issubset((0, 1))), "Wrong metadata name"
+        assert np.all(item["input"].shape == (128, 160, 128)), "Wrong image shape"
+        assert set(np.unique(item["input"])).issubset({0, 1}), "Wrong values in skeleton"
+        assert item["input"].dtype == np.float32, "Wrong data type"
+    
+    for split, n in zip(("train", "validation", "test_intra", "test"),
+                        (831, 101, 106, 131)):
+        bddataset = ClinicalDataset(dataset="bd", split=split,
+                                    target_mapping={"bd": 1, "psychotic bd": 1, "bipolar disorder": 1, "control": 0})
+        assert len(bddataset) == n, "Wrong number of subjects"
+        item = bddataset[54]
+        assert np.all(set(bddataset.target).issubset((0, 1))), "Wrong metadata name"
+        assert np.all(item["input"].shape == (128, 160, 128)), "Wrong image shape"
+        assert set(np.unique(item["input"])).issubset({0, 1}), "Wrong values in skeleton"
+        assert item["input"].dtype == np.float32, "Wrong data type"
+    
+    for split, n in zip(("train", "validation", "test_intra", "test"),
+                        (928, 116, 118, 130)):
+        sczdataset = ClinicalDataset(dataset="scz", split=split,
+                                    target_mapping={"scz": 1, "control": 0})
+        assert len(sczdataset) == n, "Wrong number of subjects"
+        item = sczdataset[54]
+        assert np.all(set(sczdataset.target).issubset((0, 1))), "Wrong metadata name"
+        assert np.all(item["input"].shape == (128, 160, 128)), "Wrong image shape"
+        assert set(np.unique(item["input"])).issubset({0, 1}), "Wrong values in skeleton"
+        assert item["input"].dtype == np.float32, "Wrong data type"
