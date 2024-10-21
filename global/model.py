ea9d4e43e9a1c82324dc6456fc15fae8a242b565
PierreAuriau <p.auriau@hotmail.fr>
2024-10-21 17:16:55 +0200

    add finetuning method

--- global/model.py
+++ global/model.py
@@ -21,6 +21,7 @@ from sklearn.metrics import balanced_accuracy_score, mean_absolute_error, \
                             mean_squared_error, r2_score, roc_auc_score
 
 from densenet import densenet121
+from classifier import Classifier
 from loss import BarlowTwinsLoss
 from log import TrainLogger
 
@@ -29,40 +30,56 @@ logging.setLoggerClass(TrainLogger)
 
 class BTModel(nn.Module):
 
-    def __init__(self, n_embedding=256):
+    def __init__(self, n_embedding=256, projector=False, classifier=False):
         super().__init__()
         
         self.n_embedding = n_embedding
         self.encoder = densenet121(n_embedding=n_embedding, in_channels=1)
-        self.projector = nn.Sequential(nn.Linear(n_embedding, 2 * n_embedding),
-                                       nn.BatchNorm1d(2 * n_embedding),
-                                       nn.ReLU(),
-                                       nn.Linear(2 * n_embedding, n_embedding))
-        self.logger = logging.getLogger("btmodel")
+        if projector:
+            self.projector = nn.Sequential(nn.Linear(n_embedding, 2 * n_embedding),
+                                        nn.BatchNorm1d(2 * n_embedding),
+                                        nn.ReLU(),
+                                        nn.Linear(2 * n_embedding, n_embedding))
+            self.logger = logging.getLogger("btmodel")
+        else:
+            self.projector = None
+        if classifier:
+            self.classifier = Classifier(latent_dim=n_embedding,
+                                         activation="sigmoid")
+            self.logger = logging.getLogger("classifier")
+        else:
+            self.classifier = None
+        if self.logger is None:
+            self.logger = logging.getLogger("encoder")
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.logger.info(f"Device used : {self.device}")
         self = self.to(self.device)
 
     def forward(self, x):
-        z = self.encoder(x) 
-        proj = self.projector(z)
-        return proj
+        z = self.encoder(x)
+        if self.projector is not None:
+            proj = self.projector(z)
+            return proj
+        if self.classifier is not None:
+            out = self.classifier(z)
+            return out
+        return z
     
     def configure_optimizers(self, **kwargs):
         return optim.Adam(self.parameters(), **kwargs)
     
     def fit(self, train_loader, val_loader, nb_epochs, 
-            correlation, lambda_param,
+            correlation_bt, lambda_bt,
             chkpt_dir, **kwargs_optimizer):
         
         self.optimizer = self.configure_optimizers(**kwargs_optimizer)
         self.lr_scheduler = None
-        self.save_hyperparameters(chkpt_dir, {"lambda_param": lambda_param, 
-                                              "correlation": correlation})
+        self.save_hyperparameters(chkpt_dir, {"lambda_bt": lambda_bt, 
+                                              "correlation_bt": correlation_bt})
         self.logger.reset_history()
         self.scaler = GradScaler()
-        self.loss_fn = BarlowTwinsLoss(correlation=correlation,
-                                       lambda_param=lambda_param / float(self.n_embedding))
+        self.loss_fn = BarlowTwinsLoss(correlation=correlation_bt,
+                                       lambda_param=lambda_bt / float(self.n_embedding))
         for epoch in range(nb_epochs):
             self.logger.info(f"Epoch: {epoch}")
             # Training
@@ -93,14 +110,14 @@ class BTModel(nn.Module):
             if epoch % 10 == 0:
                 self.logger.info(f"Loss: train: {train_loss:.2g} / val: {val_loss:.2g}")
                 self.logger.info(f"Training duration: {self.logger.get_duration()}")
-                self.save_chkpt(os.path.join(chkpt_dir,
-                                             f'barlowtwins_ep-{epoch}.pth'),
-                                             save_optimizer=True)
+                self.save_chkpt(chkpt_dir=chkpt_dir,
+                                filename=f'barlowtwins_ep-{epoch}.pth',
+                                save_optimizer=True)
                 self.logger.save(chkpt_dir, filename="_train")
         
         self.logger.save(chkpt_dir, filename="_train")
-        self.save_chkpt(os.path.join(chkpt_dir,
-                                     f'barlowtwins_ep-{epoch}.pth'),
+        self.save_chkpt(chkpt_dir=chkpt_dir,
+                        filename=f'barlowtwins_ep-{epoch}.pth',
                         save_optimizer=True)
         self.logger.info(f"End of training: {self.logger.get_duration()}")
 
@@ -124,75 +141,140 @@ class BTModel(nn.Module):
             loss = self.loss_fn(zp_1, zp_2)
         return loss.item()
     
-    def get_embeddings(self, loader):
+    def get_embeddings(self, loader, label=None):
         embeddings = []
+        labels = []
         for batch in loader:
-            x = batch["skeleton"].to(self.device) 
+            x = batch["input"].to(self.device) 
             z = self.encoder(x)
             embeddings.extend(z.cpu().numpy())
+            if label is not None:
+                labels.extend(batch[label].numpy())
+        if label is not None:
+            return np.asarray(embeddings), np.asarray(labels)
         return np.asarray(embeddings)
-
-    def test(self, loader, epoch, preproc, label, chkpt_dir, save_y_pred=False):
-        # FIXME
-        """
-        self.load_chkpt(os.path.join(chkpt_dir,
-                                f'dlmodel_preproc-{preproc}_fold-{fold}_ep-{epoch}.pth'))
-        test_loss = 0
+    
+    def test_linear_probe(self, train_loader, test_loader, epochs, label, chkpt_dir, save_y_pred=False):
         self.logger.reset_history()
-        self.logger.step()
-        y_pred, y_true = [], []
-        for batch in tqdm(loader, desc="test"):
-            inputs = batch[preproc].to(self.device)
-            targets = batch[label].to(self.device)
-            with torch.no_grad():
-                outputs = self(inputs)
-                loss = self.loss_fn(outputs, targets, label)
-            test_loss += loss.item()
-            y_pred.extend(outputs.squeeze().cpu().numpy())
-            y_true.extend(targets.cpu().numpy())
-        y_pred = np.asarray(y_pred)
-        y_true = np.asarray(y_true)
-        self.logger.info(f"Test loss : {test_loss:.2g}")
-        self.logger.reduce(reduce_fx="sum")
-        self.logger.store({"fold": fold, "epoch": epoch, "set": "test", "loss": test_loss})
-
-        r2 = r2_score(y_pred=y_pred, y_true=y_true)
-        mae = mean_absolute_error(y_pred=y_pred, y_true=y_true)
-        rmse = mean_squared_error(y_pred=y_pred, y_true=y_true, squared=False)
-        self.logger.store({"r2": r2, "mean_absolute_error": mae, "root_mean_squarred_error": rmse})
-        
-        metrics = {"r2": r2_score,
-                   "rmse": lambda y_pred, y_true: mean_squared_error(y_pred=y_pred, y_true=y_true, squared=False),
-                   "mae": mean_absolute_error}
-        for name, metric in metrics.items():
-            value = metric(y_pred=y_pred, y_true=y_true)
-            self.logger.store(**{name: value})
-        
+        for epoch in epochs:
+            self.load_chkpt(chkpt_dir=chkpt_dir,
+                            filename=f'barlowtwins_ep-{epoch}.pth')
+            self.eval()
+            z_train, y_train = self.get_embeddings(loader=train_loader, label=label)
+            z_test, y_test = self.get_embeddings(loader=test_loader, label=label)
+            clf = LogisticRegression(max_iter=1000)
+            clf.fit(z_train, y_train)
+            y_pred = clf.predict_proba(z_test)
+            self.logger.store({"epoch": epoch,
+                               "label": label,
+                               "roc_auc": roc_auc_score(y_score=y_pred[:, 1], y_true=y_test),
+                               "balanced_accuracy": balanced_accuracy_score(y_pred=y_pred.argmax(axis=1), y_true=y_test)})
+            if save_y_pred:
+                np.save(os.path.join(chkpt_dir, f"y_pred_label-{label}_epoch-{epoch}.npy"), y_pred)
         self.logger.save(chkpt_dir, filename="_test")
-        if save_y_pred:
-            np.save(os.path.join(chkpt_dir, f"y_pred_fold-{fold}_epoch-{epoch}_test.npy"), y_pred)
-            np.save(os.path.join(chkpt_dir, f"y_true_fold-{fold}_epoch-{epoch}_test.npy"), y_true)
-        """
+
+    def fine_tuning(self, train_loader, val_loader,
+                    pretrained_epoch, nb_epochs, chkpt_dir, 
+                    **kwargs_optimizer):
+        
+        self.load_chkpt(chkpt_dir=chkpt_dir, 
+                        filename=f'barlowtwins_ep-{pretrained_epoch}.pth')
+        self.encoder.requires_grad_(False) # freeze encoder
+        self.optimizer = optim.Adam(self.classifier.parameters(), **kwargs_optimizer)
+        self.lr_scheduler = None
+        self.scaler = GradScaler()
+        self.logger.reset_history()
+        self.loss_fn = nn.BCEWithLogitsLoss()
+        for epoch in range(nb_epochs):
+            self.logger.info(f"Epoch: {epoch}")
+            # Training
+            train_loss = 0
+            self.classifier.train()
+            self.encoder.eval()
+            self.logger.step()
+            for batch in tqdm(train_loader, desc="train"):
+                input = batch["input"].to(self.device)
+                label = batch["label"].to(self.device)
+                loss = self.fine_tuning_step(input, label)
+                train_loss += loss
+            self.logger.reduce(reduce_fx="sum")
+            self.logger.store({"epoch": epoch, "set": "train", "loss": train_loss})
+            
+            # Validation
+            val_loss = 0
+            self.classifier.eval()
+            self.logger.step()
+            for batch in tqdm(val_loader, desc="Validation"):
+                input = batch["input"].to(self.device)
+                label = batch["label"].to(self.device)
+                loss = self.test_classifier_step(input, label)
+                val_loss += loss
+            self.logger.reduce(reduce_fx="sum")
+            self.logger.store({"epoch": epoch, "set": "validation", "loss": val_loss})
+
+            if epoch % 10 == 0:
+                self.logger.info(f"Loss: train: {train_loss:.2g} / val: {val_loss:.2g}")
+                self.logger.info(f"Training duration: {self.logger.get_duration()}")
+                self.save_chkpt(chkpt_dir=chkpt_dir, 
+                                filename=f'classifier_ep-{epoch}.pth')
+        
+        self.logger.save(chkpt_dir, filename="_train")
+        self.save_chkpt(chkpt_dir=chkpt_dir, 
+                        filename=f'classifier_ep-{epoch}.pth')
+        self.logger.info(f"End of training: {self.logger.get_duration()}")
+    
+    def fine_tuning_step(self, input, label):
+        # FIXME : how to deal with freezing weights ?
+        self.optimizer.zero_grad()
+        with autocast(device_type=self.device.type, dtype=torch.float16):
+            with torch.no_grad():
+                z = self.encoder(input)
+            y_pred = self.classifier(z, return_logits=True)
+            loss = self.loss_fn(y_pred.squeeze(), label)
+        self.scaler.scale(loss).backward()
+        self.scaler.step(self.optimizer)
+        self.scaler.update()
+        if self.lr_scheduler:
+            self.lr_scheduler.step()
+        return loss.item()
+
+    def test_classifier_step(self, input, label):
+        with torch.no_grad():
+            z = self.encoder(input)
+            y_pred = self.classifier(z)
+            loss = self.loss_fn(y_pred.squeeze(), label)
+        return loss.item()
 
     def save_hyperparameters(self, chkpt_dir, hp={}):
         hp = {"n_embedding": self.n_embedding, **hp}
         with open(os.path.join(chkpt_dir, "hyperparameters.json"), "w") as f:
             json.dump(hp, f)
 
-    def save_chkpt(self, filename, save_optimizer=False):
-        torch.save({"encoder": self.encoder.state_dict(),
-                    "projector": self.projector.state_dict()},
-                   filename)
+    def save_chkpt(self, chkpt_dir, filename, save_optimizer=False):
+        to_save = {"encoder": self.encoder.state_dict()}
+        if self.classifier is not None:
+            to_save["classifier"] = self.classifier.state_dict()
+        if self.projector is not None:
+            to_save["projector"] = self.project.state_dict()
+        torch.save(to_save,
+                   os.path.join(chkpt_dir, filename))
         if save_optimizer:
-            torch.save({"optimizer": self.optimizer.state_dict()},
-                        os.path.join(os.path.dirname(filename), f"optimizer.pth"))
+            torch.save(self.optimizer.state_dict(),
+                       os.path.join(chkpt_dir, "optimizer.pth"))
     
-    def load_chkpt(self, filename):
-        chkpt = torch.load(filename, weights_only=True)
+    def load_chkpt(self, chkpt_dir, filename, load_optimizer=False):
+        chkpt = torch.load(os.path.join(chkpt_dir, filename), weights_only=True)
         status = self.encoder.load_state_dict(chkpt["encoder"], strict=False)
         self.logger.info(f"Loading encoder : {status}")
-        status = self.projector.load_state_dict(chkpt["classifier"], strict=False)
-        self.logger.info(f"Loading projector : {status}")
+        if (self.projector is not None) & ("projector" in chkpt.keys()):
+            status = self.projector.load_state_dict(chkpt["projector"], strict=False)
+            self.logger.info(f"Loading projector : {status}")
+        if ("classifier" in chkpt.keys()) & (self.classifier is not None):
+            status = self.projector.load_state_dict(chkpt["classifier"], strict=False)
+            self.logger.info(f"Loading classifier : {status}")
+        if load_optimizer:
+            self.optimizer.load_state_dict(torch.load(os.path.join(chkpt_dir, "optimizer.pth"),
+                                                      weights_only=True))
 
 if __name__ == "__main__":
     model = BTModel()
