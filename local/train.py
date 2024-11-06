f66b5df529310657e10765e09a0fd2b2e721ad4c
PierreAuriau <p.auriau@hotmail.fr>
2024-11-06 17:59:57 +0100

    update with new datamanager parameters

--- local/train.py
+++ local/train.py
@@ -5,6 +5,7 @@ import os
 import sys
 import argparse
 import logging
+import numpy as np
 
 # from project
 from log import setup_logging
@@ -27,12 +28,20 @@ def train_dl_model(chkpt_dir, dataset, area):
     model = DLModel()
 
     datamanager = DataManager(dataset=dataset, area=area,
-                              label="diagnosis", batch_size=config.batch_size, 
-                              num_workers=config.num_workers)
-    train_loader = datamanager.get_dataloader(split="train")
-    val_loader = datamanager.get_dataloader(split="validation")
-    test_intra_loader = datamanager.get_dataloader(split="test_intra")
-    test_loader = datamanager.get_dataloader(split="test")
+                              label="diagnosis")
+    train_loader = datamanager.get_dataloader(split="train",
+                                              batch_size=config.batch_size,
+                                              shuffle=True, 
+                                              num_workers=config.num_workers)
+    val_loader = datamanager.get_dataloader(split="validation",
+                                            batch_size=60,
+                                            num_workers=config.num_workers)
+    test_intra_loader = datamanager.get_dataloader(split="test_intra",
+                                                   batch_size=60,
+                                                   num_workers=config.num_workers)
+    test_loader = datamanager.get_dataloader(split="test",
+                                             batch_size=60,
+                                             num_workers=config.num_workers)
         
     model.fit(train_loader, val_loader,
               nb_epochs=config.nb_epochs, chkpt_dir=chkpt_dir,
@@ -64,14 +73,22 @@ def test_l2_regularisation():
     for dataset in ("scz", "bd"):
         for area in config.areas[10:15]:
             datamanager = DataManager(dataset=dataset, area=area,
-                            label="diagnosis", batch_size=64, 
-                            num_workers=8)
+                                      label="diagnosis")
             chkpt_dir_dt = os.path.join(chkpt_dir, dataset, area)
             os.makedirs(chkpt_dir_dt, exist_ok=True)
-            train_loader = datamanager.get_dataloader(split="train")
-            val_loader = datamanager.get_dataloader(split="validation")
-            test_intra_loader = datamanager.get_dataloader(split="test_intra")
-            test_loader = datamanager.get_dataloader(split="test")
+            train_loader = datamanager.get_dataloader(split="train",
+                                                      shuffle=True,
+                                                      batch_size=64, 
+                                                      num_workers=8)
+            val_loader = datamanager.get_dataloader(split="validation",
+                                                    batch_size=60, 
+                                                    num_workers=8)
+            test_intra_loader = datamanager.get_dataloader(split="test_intra",
+                                                           batch_size=60, 
+                                                           num_workers=8)
+            test_loader = datamanager.get_dataloader(split="test",
+                                                     batch_size=60, 
+                                                     num_workers=8)
             for weight_decay in (5e-4, 5e-3, 5e-2, 5e-1, 5, 5e1, 5e2):
                 print("\n" + "-"*(35+len(dataset)+len(area)+len(str(weight_decay))) +"\n")
                 print(f"dataset: {dataset} - area: {area} - weight_decay: {weight_decay}")
@@ -93,7 +110,39 @@ def test_l2_regularisation():
                             logs={"area": area,
                                   "weight_decay": weight_decay,
                                   "dataset": dataset})
+
+def test_linear_probe():
     
+    for dataset in config.datasets:
+        epoch = 99
+        chkpt_dir = os.path.join(config.path2models, 
+                                "20241102_local_models_high_weight_decay", 
+                                dataset)
+        model = DLModel()
+        datamanager = DataManager(dataset=dataset, area=config.areas[0],
+                                  label="diagnosis")
+        predictions = {}
+        labels = {}
+        for split in ("train", "validation", "test_intra", "test"):
+            predictions[split] = np.stack([
+                np.load(os.path.join(chkpt_dir,
+                                    area,
+                                    f"y_pred_ep-{epoch}_set-{split}.npy"))
+                for area in config.areas], 
+                axis=1)
+            # in case of drop_last
+            labels[split] = datamanager.dataset[split].target[:len(predictions[split])]
+
+        model.test_linear_probe(predictions=predictions,
+                                labels=labels,
+                                epoch=epoch,
+                                chkpt_dir=chkpt_dir,
+                                logs={"label": "diagnosis",
+                                    "dataset": dataset},
+                                save_y_pred=True)
+
+
+
 def parse_args(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--chkpt_dir", required=True, type=str,
@@ -106,17 +155,8 @@ def parse_args(argv):
 
 
 if __name__ == "__main__":
-    """
+    
     args = parse_args(sys.argv[1:])
-    chkpt_dir = os.path.join(config.path2models, args.chkpt_dir)
-    os.makedirs(chkpt_dir, exist_ok=True)
-    setup_logging(level="info", logfile=os.path.join(chkpt_dir, 
-                                                     "train_dl_model.log"))
-    dataset = args.dataset
-    for area in config.areas:
-        logger.info(f"### AREA: {area}")
-        train_dl_model(chkpt_dir=chkpt_dir,
-                       dataset=dataset, area=area)
-    """
-
-    test_l2_regularisation()
+    # train_all_dl_models(chkpt_dir=args.chkpt_dir)
+    # test_l2_regularisation()
+    test_linear_probe()
